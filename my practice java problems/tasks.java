f1feb087491e003cc083bc430c5bdb63a58826aa
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-02-25 18:46:39 +0530

    Add files via upload

---
+++ my practice java problems/tasks.java
@@ -0,0 +1,26 @@
+public class tasks {
+    String name;
+    int eid;
+    double salary;
+    EmployeeAssignment2(){
+
+    }
+    EmployeeAssignment2(String name){
+        this.name=name;
+    }
+    EmployeeAssignment2(String name,int eid){
+        this.name=name;
+        this.eid=eid;
+    }
+    EmployeeAssignment2(String name,int eid,double salary){
+        this.name=name;
+        this.eid=eid;
+        this.salary=salary;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Dhanushya muthumani");
+        System.out.println(2);
+        System.out.println(80000);
+    }
+}
