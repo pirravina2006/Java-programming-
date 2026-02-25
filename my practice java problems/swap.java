6e6e8fccea4ce40960ac9e0c238d5f9fcef7ea33
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-02-25 18:47:04 +0530

    Add files via upload

---
+++ my practice java problems/swap.java
@@ -0,0 +1,12 @@
+class swap{
+    public static void main(String[]args){
+        int a=5;
+        int b=10;
+        int temp=0;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping: a = " + a + ", b = " +b);
+    }
+}
