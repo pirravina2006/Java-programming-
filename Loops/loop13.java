58087b18552ad33b07ed20a0430e81f5f7e66aac
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-03-02 20:18:36 +0530

    loop13.java

--- Loops/loop13.java
+++ Loops/loop13.java
@@ -5,6 +5,7 @@ public class loop13 {
         int n=sc.nextInt();
         int count1=0;
         int count2=0;
+        double Eligibility=0;
         for(int i=1;i<=n;i++){
             String doname=sc.next();
             int age=sc.nextInt();
@@ -14,6 +15,7 @@ public class loop13 {
             if((age>=18 && age<=65) && (weight>=50) && (hemo>=12.5)){
                 sts="Eligible";
                 count1++;
+                Eligibility++;
                 System.out.println("Donor : "+doname);
                 System.out.println("Age : "+age);
                 System.out.println("Weight : "+weight);
@@ -28,14 +30,22 @@ public class loop13 {
                 System.out.println("Weight : "+weight);
                 System.out.println("Hemoglobin : "+hemo+" g/DL");
                 System.out.println("Eligibility : "+sts);
-                System.out.println("Reason : Hemoglobin is below 12.5g/DL");
+                if(hemo<12.5){
+                    System.out.println("Reason : Hemoglobin is below 12.5g/DL");
+                }
+                else if(age<18){
+                    System.out.println("Reason : Age is below 18");
+                }
+                else{
+                    System.out.println("Reason : weight is below 50kg");
+                }
             }
         }
-        double rate=(count1/n)*100;
+        Eligibility=(Eligibility/n)*100;
         System.out.println("Total Donors : "+n);
         System.out.println("Eligible donors : "+count1);
         System.out.println("Rejected donors : "+count2);
-        System.out.printf("Eligibility rate : %.2f",rate," %");
+        System.out.printf("Eligibility rate : %.2f",Eligibility," %");
         sc.close();
     }
 }
