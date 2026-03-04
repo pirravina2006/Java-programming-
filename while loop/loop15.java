ca6f4535c0452d1b551a7b6375d79e5bae7f2943
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-03-04 10:25:10 +0530

    Add files via upload

---
+++ while loop/loop15.java
@@ -0,0 +1,49 @@
+// import java.util.Scanner; while loop collatz sequence
+// public class loop15 {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int count=0;
+//         while(n!=1){
+//             System.out.print(n+" ");
+//             if(n%2==0){
+//                 n=n/2;
+//             }
+//             else{
+//                 n=3*n+1;
+//             }
+//             count++;
+//         }
+//         System.out.print(1);
+//         System.out.println("\nSteps : "+count);
+//         sc.close();
+//     }
+// }
+
+
+// do while loop collatz sequence
+import java.util.Scanner;
+public class loop15 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        System.out.println("Collatz sequence starting from "+n);
+        do{
+            if(n==1){
+                break;
+            }
+            System.out.print(n+" ");
+            if(n%2==0){
+                n=n/2;
+            }
+            else{
+                n=(3*n)+1;
+            }
+            count++;
+        }while(n!=1);
+        System.out.print(1);
+        System.out.println("\nSteps : "+count);
+        sc.close();
+    }
+}
