999ea3aaa516c4cc014d0ad7e2ca152886d6a6be
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-03-04 15:03:30 +0530

    loop23.java

--- while loop/loop23.java
+++ while loop/loop23.java
@@ -4,17 +4,36 @@ public class loop23 {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
         int count=0;
-        for(int i=0;i<str.length();i++){
+        int i=0;
+        do{
             char ch=str.charAt(i);
-            if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'||
-                ch=='a' ||ch=='e' ||ch=='i'||ch=='o'||ch=='u')
-            {
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
                 count++;
             }
-        }
-        System.out.println();
-        System.out.println("Text : "+str);
-        System.out.println("Vowel count : "+count);
+            i++;
+        }while(i<str.length());
+        System.out.println(count);
         sc.close();
     }
 }
+
+// import java.util.Scanner;
+// public class loop23 {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String str=sc.nextLine();
+//         int count=0;
+//         for(int i=0;i<str.length();i++){
+//             char ch=str.charAt(i);
+//             if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'||
+//                 ch=='a' ||ch=='e' ||ch=='i'||ch=='o'||ch=='u')
+//             {
+//                 count++;
+//             }
+//         }
+//         System.out.println();
+//         System.out.println("Text : "+str);
+//         System.out.println("Vowel count : "+count);
+//         sc.close();
+//     }
+// }
