f53f35c77809105e5f3126affeb5ddcd126ec841
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-03-06 09:21:47 +0530

    Add files via upload

---
+++ nested loop/pro20.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class pro20 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int [][]arr=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+
+        System.out.println("Original Matrix : ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Transposed Matrix :");
+        for(int j=0;j<c;j++){
+            for(int i=0;i<r;i++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
