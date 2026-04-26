81f9ca5ca2c72697a2c4785ce604bf6770740c96
Pirravina Ravi <pirravinadhanalakshmi@gmail.com>
2026-04-26 11:41:20 +0530

    Add files via upload

---
+++ operators/pro15.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class pro15 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int res=a^b;
+        System.out.println(res);
+        sc.close();
+    }
+}
